da42396ec21295ef4b7ae5f0f15ebd9566a84f89
lasse <77295879+lassejlv@users.noreply.github.com>
2024-05-05 20:37:57 +0200

    Changes

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,5 +1,32 @@
 import { Hono } from "hono";
-
+import { getUser, kindeClient, sessionManager } from "@/util/kinde";
 const router = new Hono();
+
+router.get("/login", async (c) => {
+  const loginUrl = await kindeClient.login(sessionManager(c));
+  return c.redirect(loginUrl.toString());
+});
+
+router.get("/register", async (c) => {
+  const registerUrl = await kindeClient.register(sessionManager(c));
+  return c.redirect(registerUrl.toString());
+});
+
+router.get("/callback", async (c) => {
+  // get called eveyr time we login or register
+  const url = new URL(c.req.url);
+  await kindeClient.handleRedirectToApp(sessionManager(c), url);
+  return c.redirect("/");
+});
+
+router.get("/logout", async (c) => {
+  const logoutUrl = await kindeClient.logout(sessionManager(c));
+  return c.redirect(logoutUrl.toString());
+});
+
+router.get("/me", getUser, async (c) => {
+  const user = c.var.user;
+  return c.json({ user });
+});
 
 export default router;
